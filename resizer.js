ec94311c9c5d0cba26f653ed31a4146216eb46ff
William Golden <egdelwonk@gmail.com>
2012-03-13 08:20:05 -0500

    Bugfix for issue #5 - cleaned references to $

--- resizer.js
+++ resizer.js
@@ -16,49 +16,49 @@
 			'small_tablet': {'order': 1,'description': 'Small Tablet', 'width': 460, 'height': 640},
 			'tablet_portrait': {'order': 2,'description': 'Tablet - Portrait','width': 768, 'height': 1024},
 			'tablet_landscape': {'order': 3,'description': 'Tablet - Landscape','width': 1024, 'height': 768},
-			'desktop': {'order': 4,'description': 'Desktop', 'width': 1200, 'height': 800},
+			'desktop': {'order': 4,'description': 'Desktop', 'width': 1200, 'height': 800}
 		},
 		init: function(){
 			Resizer.loadCss();
-			Resizer.$target = $('body');
+			Resizer.$target = jQuery('body');
 			Resizer.dimensions.height = Resizer.$target.height();
 			Resizer.dimensions.width = Resizer.$target.width();
-			Resizer.$container = $('<div id="responsive_resize_container"></div>').appendTo('body');
-			$('<div class="resize_inner"><a class="resizer_bigger" href="#">+</a><a class="resizer_smaller" href="#">-</a><a class="resize_drop_selector" href="#"><span class="resizer_active_dimension">Mobile <span class="resizer_dimension">(320x480)</span></span></a><ul class="resize_dropdown"></ul><a class="reload" href="#">Reload</a><a class="reset" href="#">Reset</a></div>').appendTo(Resizer.$container);
-			Resizer.$container_dropdown = $('ul.resize_dropdown', Resizer.$container);
-			$.each(Resizer.sizes, function(index, value){
-				$('<li data-dimension="'+ index +'" data-description="'+ value.description +'" data-height="'+ value.height +'" data-width="'+ value.width +'"><a href="#">'+ value.description +'</a></li>').appendTo(Resizer.$container_dropdown);
+			Resizer.$container = jQuery('<div id="responsive_resize_container"></div>').appendTo('body');
+			jQuery('<div class="resize_inner"><a class="resizer_bigger" href="#">+</a><a class="resizer_smaller" href="#">-</a><a class="resize_drop_selector" href="#"><span class="resizer_active_dimension">Mobile <span class="resizer_dimension">(320x480)</span></span></a><ul class="resize_dropdown"></ul><a class="reload" href="#">Reload</a><a class="reset" href="#">Reset</a></div>').appendTo(Resizer.$container);
+			Resizer.$container_dropdown = jQuery('ul.resize_dropdown', Resizer.$container);
+			jQuery.each(Resizer.sizes, function(index, value){
+				jQuery('<li data-dimension="'+ index +'" data-description="'+ value.description +'" data-height="'+ value.height +'" data-width="'+ value.width +'"><a href="#">'+ value.description +'</a></li>').appendTo(Resizer.$container_dropdown);
 			});
-			$('a.resize_drop_selector').click(function(e){
+			jQuery('a.resize_drop_selector').click(function(e){
 				Resizer.$container_dropdown.show();
 				e.preventDefault();
 			});
 			
-			$('a.resizer_smaller', Resizer.$container).click(function(e){
+			jQuery('a.resizer_smaller', Resizer.$container).click(function(e){
 				Resizer.smaller();
 				e.preventDefault();
 			});
-			$('a.resizer_bigger', Resizer.$container).click(function(e){
+			jQuery('a.resizer_bigger', Resizer.$container).click(function(e){
 				Resizer.bigger();
 				e.preventDefault();
 			});
 			
-			$('a.reset', Resizer.$container).click(function(e){
+			jQuery('a.reset', Resizer.$container).click(function(e){
 				Resizer.reset();
 				e.preventDefault();
 			});
 			
-			$('a.reload', Resizer.$container).click(function(e){
+			jQuery('a.reload', Resizer.$container).click(function(e){
 				Resizer.reload();
 				e.preventDefault();
 			});
 			
-			$('li a', Resizer.$container_dropdown).click(function(e){
-				$data = $(this).parent().data();
+			jQuery('li a', Resizer.$container_dropdown).click(function(e){
+				$data = jQuery(this).parent().data();
 				Resizer.resize($data.width, $data.height);
 				Resizer.$container_dropdown.hide();
 				Resizer.current_dimension = $data.dimension;
-				$('.resizer_active_dimension').html(  $data.description +' <span class="resizer_dimension">('+ $data.width +'x'+ $data.height +')</span>');
+				jQuery('.resizer_active_dimension').html(  $data.description +' <span class="resizer_dimension">('+ $data.width +'x'+ $data.height +')</span>');
 				e.preventDefault();
 			});
 		},
@@ -66,9 +66,9 @@
 		
 		//resize the window size to selected size
 		resize: function(width, height){
-			$('#resizer_resize_wrapper').remove();
-			$wrapper = $('<div id="resizer_resize_wrapper"></div>').appendTo(Resizer.$target);
-			$('<iframe />', {
+			jQuery('#resizer_resize_wrapper').remove();
+			$wrapper = jQuery('<div id="resizer_resize_wrapper"></div>').appendTo(Resizer.$target);
+			jQuery('<iframe />', {
 				name: 'resizer_frame',
 				id:   'resizer_frame',
 				class: 'resizer_active',
@@ -84,12 +84,12 @@
 		},
 		//reset window to original size
 		reset: function(){
-			$('#resizer_resize_wrapper').remove();
+			jQuery('#resizer_resize_wrapper').remove();
 		},
 		
 		//reload current url inside of the iframe - avoid having to refresh and reinitialize bookmarklet while developing a site
 		reload: function(){
-			$('#resizer_frame').attr('src', window.location + '?' + Math.floor(Math.random()*11));
+			jQuery('#resizer_frame').attr('src', window.location + '?' + Math.floor(Math.random()*11));
 		},
 		//increase the window size to the next largest resolution available
 		bigger: function(){
@@ -98,7 +98,7 @@
 				next_dimension = Resizer.sizes[Resizer.sizes_order[current_order + 1]];
 				dimensions = {width: next_dimension.width, height: next_dimension.height};
 				Resizer.resize(dimensions.width, dimensions.height);
-				$('.resizer_active_dimension').html(  next_dimension.description +' <span class="resizer_dimension">('+ dimensions.width +'x'+ dimensions.height +')</span>');
+				jQuery('.resizer_active_dimension').html(  next_dimension.description +' <span class="resizer_dimension">('+ dimensions.width +'x'+ dimensions.height +')</span>');
 				Resizer.current_dimension = Resizer.sizes_order[next_dimension.order];
 			}
 		},
@@ -109,14 +109,14 @@
 				next_dimension = Resizer.sizes[Resizer.sizes_order[current_order - 1]];
 				dimensions = {width: next_dimension.width, height: next_dimension.height};
 				Resizer.resize(dimensions.width, dimensions.height);
-				$('.resizer_active_dimension').html(  next_dimension.description +' <span class="resizer_dimension">('+ dimensions.width +'x'+ dimensions.height +')</span>');
+				jQuery('.resizer_active_dimension').html(  next_dimension.description +' <span class="resizer_dimension">('+ dimensions.width +'x'+ dimensions.height +')</span>');
 				Resizer.current_dimension = Resizer.sizes_order[next_dimension.order];
 			}
 
 		},
 		loadCss: function(){
-			if($('head link#responsive_css').length == 0){						
-				$('<link>',{
+			if(jQuery('head link#responsive_css') != null){						
+				jQuery('<link>',{
 					href: 'http://codebomber.com/jquery/resizer/resizer.css',
 					rel: 'stylesheet',
 					id: 'responsive_css'
